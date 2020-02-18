e912d5103f77f3fff5db6bb50c354551a39c842b
Ozyrys24 <48652183+Ozyrys24@users.noreply.github.com>
2020-02-18 22:18:37 +0100

    Bonus Task 5 Complited
    Bonus Task 5 Complited

--- 1_Zadania/dodatkowe/5_Tooltips/js/app.js
+++ 1_Zadania/dodatkowe/5_Tooltips/js/app.js
@@ -1,3 +1,21 @@
-/*
- <span class="tooltipText">Text tooltipa</span>
- */
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("działa");
+    var tooltips = document.getElementsByClassName("tooltip");
+    console.log(tooltips);
+    var span;
+    for(let i=0; i<tooltips.length; i++) {
+        tooltips[i].addEventListener("mouseover", function() {
+            span = document.createElement("span");
+            span.className = "tooltipText";
+            span.textContent = tooltips[i].getAttribute("data-text");
+            tooltips[i].appendChild(span);
+            console.log(tooltips[i].getAttribute("data-text"));
+        })
+        tooltips[i].addEventListener("mouseout", function() {
+            tooltips[i].removeChild(span);
+            console.log("mouse out");
+        })
+    }
+})
+//Strona wyswitla podpowiedzi po najechaniu na tekst w tooltipie.
+//Działa według założeń
