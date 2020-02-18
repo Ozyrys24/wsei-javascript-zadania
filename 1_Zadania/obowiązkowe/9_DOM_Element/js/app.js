9e59803f93956fc380fde9d8631c5d3a466d55ab
Ozyrys24 <48652183+Ozyrys24@users.noreply.github.com>
2020-02-18 10:42:58 +0100

    Task 9 complited
    Task 9 complited

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -9,4 +9,86 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+    //Task 0
+    function getDataInfo(links) {
+        var linksArray = [];
+
+        for (var i = 0; i < links.length; i++)
+            linksArray.push(links[i].getAttribute(links[i].getAttributeNames('data')));
+
+        return linksArray;
+    }
+    console.log(getDataInfo(links));
+
+    //Task 1
+    var items = [];
+    var itemsWithChild = [];
+    items.push(homeElement, childElements, banner, blocks, links);
+
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].length !== undefined) {
+            for (var j = 0; j < items[i].length; j++) {
+                itemsWithChild.push(items[i][j]);
+                console.log("item: " + items[i][j] + " has class: " + items[i][j].className
+                    + " and tag: " + items[i][j].tagName);
+            }
+        }
+        else {
+            itemsWithChild.push(items[i]);
+            console.log("item: " + items[i] + " has class: " + items[i].className
+                + " and tag: " + items[i].tagName);
+        }
+    }
+    console.log(itemsWithChild);
+
+    //Task 2
+    for (var i = 0; i < blocks.length; i++) {
+        console.log(blocks[i].innerHtml);
+        console.log(blocks[i].outerHTML);
+        blocks[i].innerHtml = "Nowa zawartość diva o klasie blocks";
+        console.log(blocks[i].innerHtml);
+    }
+    //InnerHTML daje dostęp do zawartości tekstowej diva
+    //OuterHTML za to do kodu diva
+
+    //Task 3
+    var el = document.getElementById('mainFooter');
+
+    function getId(element) {
+        return element.id;
+    }
+    console.log(getId(el));
+
+    //Task 4
+    function getTags(elements) {
+        var tags = [];
+        for (var i = 0; i < elements.length; i++)
+            tags.push(elements[i].tagName);
+
+        return tags;
+    }
+    console.log(getTags(childElements));
+
+    //Task 5
+    function getClassInfo(element) {
+        var classes = [];
+        classes = element.classList;
+        return classes;
+    }
+
+    console.log(getClassInfo(banner));
+
+    //Task 6
+    var liElements = document.getElementsByTagName('nav')[0].querySelectorAll('li');
+
+    function setDataDirection(elements) {
+        for (var i = 0; i < elements.length; i++) {
+            if (!elements[i].getAttribute('data-direction')) {
+                elements[i].setAttribute('data-direction', 'top');
+            }
+        }
+        return elements;
+    }
+    setDataDirection(liElements);
 });
+
