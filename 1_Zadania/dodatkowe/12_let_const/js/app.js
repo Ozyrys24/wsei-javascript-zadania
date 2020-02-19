0b6a1d3fa473753aa609de79bd219f46524c1973
Ozyrys24 <48652183+Ozyrys24@users.noreply.github.com>
2020-02-19 15:09:05 +0100

    Bonus Task 12 Complited
    Bonus Task 12 Complited

--- 1_Zadania/dodatkowe/12_let_const/js/app.js
+++ 1_Zadania/dodatkowe/12_let_const/js/app.js
@@ -26,4 +26,65 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     console.log(z, y);
+
+    //Task 0
+    const a = 5;
+    let b = 5;
+
+    for(let i=0; i<1; i++) {
+        //error a = 3;
+        b = 3; //nadpisało
+        console.log("Zasięg const w pętli", a);
+        console.log("Zasięg let w pętli", b);
+    }
+    function Test(a, b) {
+        a = 2; //nadpisało
+        b = 2; //nadpisało
+        console.log("Zasięg const w funkcji", a);
+        console.log("Zasięg let w funkcji", b);
+    }
+    Test(a,b);
+
+    {
+        b = 4; //nie zmieniło się
+        c = 4; //nadpisało
+        console.log("Zasięg const w bloku", a);
+        console.log("Zasięg let w bloku", b);
+    }
+
+    //Task 1
+    require("./zadanie01");
+
+    //Task 2
+    require("./zadanie02");
+
+    //Task 3
+    for(var i=0; i<=11; i++)
+        console.log(i);
+
+    console.log(i); //zmienna i została wypisana zwiększona o 1 poza pętlą
+
+    for(let j=0; j<=11; j++)
+        console.log(j);
+
+    //console.log(j); //komunikat o niezdefiniowanej zmiennej j
+    //Task 4
+    require("./zadanie04");
+
+    //Task 5
+    (function() {
+        console.log("hello world");
+    } ());
+
+    //Task 6
+    var printName = (function(name) {
+        return {
+            getName: function() {
+                return name;
+            }
+        }
+    })("Mateusz");
+
+    console.log(printName.getName());
+
 })
